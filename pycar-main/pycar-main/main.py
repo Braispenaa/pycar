f186aef2134d73254aa61da743279e9965b32a8b
Braispenaa <braisitogallego@gmail.com>
2021-10-18 19:16:28 +0200

    Initial commit

---
+++ pycar-main/pycar-main/main.py
@@ -0,0 +1,13 @@
+# Import a library of functions called 'pygame'
+import pygame
+from main_functions import *
+
+
+# Initialize the game engine
+pygame.init()
+
+#call main routine
+size = (1300, 600)# Define size of windows
+ancho = int(input("ancho de la ventana"))
+alto = int(input("alto de la ventana"))
+
